14cd8366cd7fb885df5bdd35e635a4d7922b242c
proffg05-smart <proffg05@gmail.com>
2025-07-28 16:25:11 +0300

    newapp

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@ echo "<<<<<<<<<<< COLLECTING STATIC FILES >>>>>>>>"
 python manage.py collectstatic --noinput
 
 echo "<<<<<<<<<<< STARTING PROJECT >>>>>>>>>>>>>>"
-#gunicorn smartpace.wsgi
-daphne smartpace.asgi:application
+gunicorn smartpace.wsgi
+#daphne smartpace.asgi:application
