2da8202df69fd398703039a86206d21900ebda39
proffg05-smart <proffg05@gmail.com>
2025-09-26 23:05:47 +0300

    gk

--- core/views.py
+++ core/views.py
@@ -302,20 +302,23 @@ def profile_dashboard(request):
 def edit_profile(request):
     user = request.user
     profile = user.profile
+    success = False  
+
     if request.method == "POST":
         user_form = UserForm(request.POST, instance=user)
         profile_form = UserProfileForm(request.POST, request.FILES, instance=profile)
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
-            return redirect("profile_dashboard")
+            success = True  
     else:
         user_form = UserForm(instance=user)
         profile_form = UserProfileForm(instance=profile)
+
     return render(
         request,
         "core/edit_profile.html",
-        {"user_form": user_form, "profile_form": profile_form},
+        {"user_form": user_form, "profile_form": profile_form, "success": success}
     )
 
 
